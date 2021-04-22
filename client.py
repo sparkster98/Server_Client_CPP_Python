fc487062806156727399d26e98b7c553a8094f24
sparkster98 <tomassmyth2016@gmail.com>
2021-04-22 11:45:29 +0100

    Added comments to client.py

--- client.py
+++ client.py
@@ -3,14 +3,15 @@ import socket
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 54000        # The port used by the server
 
-
+# Create the socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     while True:
+        # Take a message from the user
+        # Send it to the server
+        # Print the response to the console
         message = input()
-        #s.sendall(b'Hello, world')
         s.send(message.encode())
         data = s.recv(1024)
         print(data.decode())
 
-#print('Received', data.decode())
